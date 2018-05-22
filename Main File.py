beca597913cd314ae642e438cbfd283469b078f4
butigard <bgutigard@gmail.com>
2018-05-22 13:34:48 -0500

    b

--- Main File.py
+++ Main File.py
@@ -1,3 +1,13 @@
-from graphing.py import *
+from graphing import *
+from scraping import *
 
-graph([['Ball', 1.5, 1.5]])
+
+test2 = scrape("http://www.brooksbaseball.net/pfxVB/pfx.php?month=5&day=9&year=2018&game=gid_2018_05_09_miamlb_chnmlb_1%2F&pitchSel=500779&prevGame=gid_2018_05_09_miamlb_chnmlb_1%2F&prevDate=59&league=mlb")
+player = []
+
+for i in range(len(test2) - 1):
+    player.append(test2[i + 1])
+
+graph(player)
+
+#graph([['Ball', 1.5, 1.5]])
